bc317b1097196e42daedb64dddadcdb608308a45
p1wer <ferrystandoff2@gmail.com>
2026-07-09 15:11:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,96 +1,111 @@
 #include <iostream>
-// #define DEBUG
+#include <cstring>
 
-int length(const char* str) {
-	int count = 0;
-	while (true) {
-		if (str[count] == '\0') {
-			return count;
-		}
-		count++;
-	}
-}
-
-class String {
+class String{
 private:
 	char* string;
 public:
-	String(const char* str) {
-#ifdef DEBUG
-		std::cout << "Constructor called\t" << this << "\n";
-#endif //DEBUG
-		this->string = new char[length(str) + 1];
-
-		for (int i = 0; i < length(str); i++) {
-			string[i] = str[i];
-		}
-
-		string[length(str)] = '\0';
+	int length = 0;
+	
+	String(){
+		string = nullptr;
 	}
-	~String() {
-#ifdef DEBUG
-		std::cout << "Destructor called\t" << this << "\n";
-#endif //DEBUG
+	String(const char* string){
+		while(string[length] != '\0'){
+			length++;
+		}
+		this->string = new char[length + 1];
+		
+		for(int i = 0; i < length; i++){
+			this->string[i] = string[i];
+		}
+		this->string[length] = '\0';
+	}
+	String(const String &other){
+		length = other.length;
+		
+		string = new char[other.length + 1];
+		
+		for(int i = 0; i < other.length; i++){
+			string[i] = other.string[i];
+		}
+		string[other.length] = '\0';
+	}
+	~String(){
 		delete[] string;
 	}
-	String(const String& other) {
-#ifdef DEBUG
-		std::cout << "Copy constructor called\t" << this << "\n";
-#endif //DEBUG
-		string = new char[length(other.string) + 1];
-
-		for (int i = 0; i < length(other.string); i++) {
-			this->string[i] = other.string[i];
-		}
-
-		string[length(other.string)] = '\0';
+	char& operator[](int index){
+		return string[index];
 	}
-
-	String& operator=(const String& other) {
-#ifdef DEBUG
-		std::cout << "Operator= was called\t" << this << "\n";
-#endif //DEBUG
-
-		if (this == &other) {
-#ifdef DEBUG
-			std::cout << "this = &other";
-#endif // DEBUG
-			return *this;
+	String& operator=(const char* string){
+		delete[] this->string;
+		int newLength = std::strlen(string);
+		length = newLength;
+		this->string = new char[newLength + 1];
+		
+		for(int i = 0; i < newLength; i++){
+			this->string[i] = string[i];
 		}
-
-
-		delete[] string;
-		this->string = new char[length(other.string) + 1];
-
-		for (int i = 0; i < length(other.string); i++) {
-			this->string[i] = other.string[i];
-		}
-		string[length(other.string)] = '\0';
+		this->string[newLength] = '\0';
 		return *this;
+		
 	}
-	String operator+(const String& other) {
-#ifdef DEBUG
-		std::cout << "Operator+ was called\t" << this << "\n";
-#endif
-		int thisLen = length(string);
-		int otherLen = length(other.string);
-
-		char* result = new char[thisLen + otherLen + 1];
-
-		for (int i = 0; i < thisLen; i++) {
-			result[i] = string[i];
+	String operator+(const String &other){
+		char* newString = new char[length + other.length + 1];
+		
+		for(int i = 0; i < length; i++){
+			newString[i] = string[i];
 		}
-		for (int i = 0; i < otherLen; i++) {
-			result[thisLen + i] = other.string[i];
+		for(int i = 0; i < other.length; i++){
+			newString[length + i] = other.string[i];
 		}
-		result[thisLen + otherLen] = '\0';
-
-		String RESULT = result;
-		delete[] result;
-
-		return RESULT;
+		newString[length + other.length] = '\0';
+		
+		String result(newString);
+		delete[] newString;
+		
+		return result;
 	}
-	void print(char sep='\n') {
-		std::cout << string << sep;
+	String operator*(int number){
+		char* newString = new char[length * number + 1];
+		
+		for(int i = 0; i < number; i++){
+			for(int k = 0; k < length; k++){
+				newString[i*length + k] = string[k];
+			}
+		}
+		newString[length * number] = '\0';
+		String result(newString);
+		delete[] newString;
+		
+		return result;
 	}
+	bool operator==(const String &other){
+		if(length != other.length){
+			return false;
+			}
+		for(int i = 0; i < length; i++){
+			if(string[i] != other.string[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+	bool operator!=(const String &other){
+		if(length != other.length){
+			return true;
+			}
+		for(int i = 0; i < length; i++){
+			if(string[i] != other.string[i]){
+				return true;
+			}
+		}
+		return false;
+	}
+	
 };
+
+int main(){
+	return 0;
+}
+
